56804c431fd3ed792cb3379153f67e03253fe126
Anastasiya-Shtukar <162809005+Anastasiya-Shtukar@users.noreply.github.com>
2024-10-09 20:49:09 +0200

    html

--- src/js/search.js
+++ src/js/search.js
@@ -4,6 +4,7 @@ import 'izitoast/dist/css/iziToast.min.css';
 const btnSearch = document.querySelector('.btn-search');
 const searchImagesInput = document.querySelector('.search-images-input');
 const inputImagesValue = searchImagesInput.value;
+const gallery = document.querySelector('.gallery');
 
 btnSearch.addEventListener('click', event => {
   event.preventDefault();
@@ -11,7 +12,7 @@ btnSearch.addEventListener('click', event => {
 
   const searchParams = new URLSearchParams({
     key: '46374353-2f98dff3c8dab99fd2b2fa1f1',
-    q: `"${inputImagesValue}"`,
+    q: `'${inputImagesValue}'`,
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: true,
@@ -32,6 +33,28 @@ btnSearch.addEventListener('click', event => {
       if (data.hits.length === 0) {
         throw new Error('No results found');
       }
+      const galleryImages = data
+        .map(
+          image => `<li class="gallery-item">
+  <a class="gallery-link" href="${image.largeImageURL}">
+    <img
+      class="gallery-image"
+      src="${image.webformatURL}"
+      data-source="${image.largeImageURL}"
+      alt="${image.tags}"
+    />
+  </a>
+  <div class="characteristics-photo">
+  <div class="likes">Likes:${image.likes}</div>
+  <div class="views">Views:${image.views}</div>
+  <div class="comments">Comments:${image.comments}</div>
+  <div class="downloads">Downloads:${image.downloads}</div>
+  </div>
+</li>`
+        )
+        .join('');
+
+      gallery.insertAdjacentHTML('beforeend', galleryImages);
     })
     .catch(error => {
       console.error(error);
