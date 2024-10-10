0436b856a94f0974946364bf6860a57275f7b1ee
Anastasiya-Shtukar <162809005+Anastasiya-Shtukar@users.noreply.github.com>
2024-10-10 15:37:17 +0000

    Deploying to gh-pages from @ Anastasiya-Shtukar/goit-js-hw-11@e918bf316a52733ae8b7c5fcd0c405398c79ea4d 🚀

--- page-2.js
+++ page-2.js
@@ -1,17 +1,17 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";import{i as c}from"./assets/vendor-I1I71QQ2.js";/* empty css                      */const n=document.querySelector(".btn-search"),r=document.querySelector(".search-images-input"),i=r.value,d=document.querySelector(".gallery");n.addEventListener("click",a=>{a.preventDefault(),console.log(r.value);const s=new URLSearchParams({key:"46374353-2f98dff3c8dab99fd2b2fa1f1",q:`'${i}'`,image_type:"photo",orientation:"horizontal",safesearch:!0});console.log(s.toString());const o=`https://pixabay.com/api/?${s}`;fetch(o,{method:"GET"}).then(e=>{if(!e.ok)throw new Error(e.status);return e.json()}).then(e=>{if(e.hits.length===0)throw new Error("No results found");const l=e.map(t=>`<li class="gallery-item">
-  <a class="gallery-link" href="${t.largeImageURL}">
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";import{S as i,i as l}from"./assets/vendor-5ObWk2rO.js";/* empty css                      */const p=document.querySelector(".btn-search"),t=document.querySelector(".search-images-input").value,r=document.querySelector(".gallery");p.addEventListener("click",c=>{c.preventDefault(),console.log(t.value);const s=new URLSearchParams({key:"46374353-2f98dff3c8dab99fd2b2fa1f1",q:t,image_type:"photo",orientation:"horizontal",safesearch:!0});console.log(s.toString());const o=`https://pixabay.com/api/?${s}`;fetch(o,{method:"GET"}).then(a=>{if(!a.ok)throw new Error(a.status);return a.json()}).then(a=>{if(a.hits.length===0)throw new Error("No results found");const n=a.hits.map(e=>`<li class="gallery-item">
+  <a class="gallery-link" href="${e.largeImageURL}">
     <img
       class="gallery-image"
-      src="${t.webformatURL}"
-      data-source="${t.largeImageURL}"
-      alt="${t.tags}"
+      src="${e.webformatURL}"
+      data-source="${e.largeImageURL}"
+      alt="${e.tags}"
     />
   </a>
   <div class="characteristics-photo">
-  <div class="likes">Likes:${t.likes}</div>
-  <div class="views">Views:${t.views}</div>
-  <div class="comments">Comments:${t.comments}</div>
-  <div class="downloads">Downloads:${t.downloads}</div>
+  <p class="characteristic">Likes:<span class="characteristic-span">${e.likes}</span></p>
+  <p class="characteristic">Views:<span class="characteristic-span">${e.views}</span></p>
+  <p class="characteristic">Comments:<span class="characteristic-span">${e.comments}</span></p>
+  <p class="characteristic">Downloads:<span class="characteristic-span">${e.downloads}</span></p>
   </div>
-</li>`).join("");d.insertAdjacentHTML("beforeend",l)}).catch(e=>{console.error(e),c.error({title:"",message:"Sorry, there are no images matching your search query. Please try again!",position:"topCenter"})})});
+</li>`).join("");r.innerHTML="",r.insertAdjacentHTML("beforeend",n),new i(".gallery a",{captionsData:"alt",captionDelay:250})}).catch(a=>{console.error(a),l.error({title:"",message:"Sorry, there are no images matching your search query. Please try again!",position:"topCenter"})})});
 //# sourceMappingURL=page-2.js.map
